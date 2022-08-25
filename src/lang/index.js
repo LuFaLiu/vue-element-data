09d13441af6c9d72a477fafdc961d6cc326603ec
LuFaLiu <320283075@qq.com>
2022-08-25 08:50:47 +0800

    adapter upload component

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import Element from 'element-ui'
 import Cookies from 'js-cookie'
 import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
 import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
 import enLocale from './en'
 import zhLocale from './zh'
+
+
+
 
 Vue.use(VueI18n)
 
@@ -28,5 +32,7 @@ const i18n = new VueI18n({
   messages,
   silentTranslationWarn: true, //去掉vue-i18n警告
 })
+
+Element.i18n((key, value) => i18n.t(key, value))
 
 export default i18n
--- src/views/systools/menutest.vue
+++ src/views/systools/menutest.vue
@@ -313,6 +313,12 @@ export default {
               }
             }]
           },
+        },
+        elupload:{
+          fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+          headers:{},
+          limit:3,
+          data:{}
         }
 
     }
@@ -328,6 +334,36 @@ export default {
 
     },
     formatTooltip(){
+
+    },
+    onRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    onPreview(file) {
+      console.log(file);
+    },
+    onExceed(files, fileList) {
+      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${ file.name }？`);
+    },
+    httpRequest(){
+
+    },
+    beforeUpload(){
+
+    },
+    onChange(){
+
+    },
+    onSuccess(){
+
+    },
+    onProgress(){
+
+    },
+    onError(){
 
     }
   }
