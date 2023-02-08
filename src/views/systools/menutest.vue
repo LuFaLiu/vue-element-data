5a8c05535b35e16b8d13e79d6e7fd04046819f96
LuFaLiu <320283075@qq.com>
2023-02-08 08:49:05 +0800

    Add the With offset type of the Notification component

--- src/views/systools/menutest.vue
+++ src/views/systools/menutest.vue
@@ -2100,6 +2100,13 @@ export default {
       }
       
     },
+    openNotificationWithOffset() {
+      this.$notify.success({
+        title: 'Success',
+        message: 'This is a success message',
+        offset: 100
+      });
+    },
     stepNext(){
       /*
       该方法会直接修改VueComponent, error:Avoid mutating a prop directly since the value will be overwritten whenever the parent component re-renders. Instead, use a data or computed property based on the prop's value. Prop being mutated: "active"
