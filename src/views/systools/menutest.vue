e22f650f9a666677419defea7a83665e37292366
LuFaLiu <320283075@qq.com>
2023-07-26 08:46:52 +0800

    Adds the Arbitrary time range type of the TimePicker component

--- src/views/systools/menutest.vue
+++ src/views/systools/menutest.vue
@@ -614,6 +614,8 @@ export default {
         value1: '',
         value2: new Date(2016, 9, 10, 18, 40),
         value3: new Date(2016, 9, 10, 18, 40),
+        value5: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
+        value6: [new Date(2016, 9, 10, 8, 40), new Date(2016, 9, 10, 9, 40)],
         
         fixedTimePickerOptions:{
           start: '08:30',
