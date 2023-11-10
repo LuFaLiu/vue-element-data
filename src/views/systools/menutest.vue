605daf72a5c0b2523d6b28b19af884b1ab2d5701
LuFaLiu <320283075@qq.com>
2023-11-10 08:50:18 +0800

    Add the Common layouts and Example types of the Container component

--- src/views/systools/menutest.vue
+++ src/views/systools/menutest.vue
@@ -3919,6 +3919,28 @@ export default {
         iconAttribute: {
           type: "iconAttribute",
         },
+        elContainerType1: {
+          tag: "span",
+          text: "Common layouts",
+          style: {
+            margin: "15px",
+            display: "block",
+          }
+        },
+        elContainerCommonLayoutType: {
+          type: 'elContainerCommonLayout'
+        },
+        elContainerType2: {
+          tag: "span",
+          text: "Example",
+          style: {
+            margin: "15px",
+            display: "block",
+          }
+        },
+        elContainerExampleType: {
+          type: 'elContainerExample'
+        },
         elAvatarType1: {
           tag: "span",
           text: "Basic",
@@ -7074,6 +7096,54 @@ export default {
 }
 
 ::v-deep {
+
+  .elContainerCommonLayout {
+    .el-header, .el-footer {
+      background-color: #B3C0D1;
+      color: #333;
+      text-align: center;
+      line-height: 60px;
+    }
+    
+    .el-aside {
+      background-color: #D3DCE6;
+      color: #333;
+      text-align: center;
+      line-height: 200px;
+    }
+    
+    .el-main {
+      background-color: #E9EEF3;
+      color: #333;
+      text-align: center;
+      line-height: 160px;
+    }
+    
+    body > .el-container {
+      margin-bottom: 40px;
+    }
+    
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+      line-height: 260px;
+    }
+    
+    .el-container:nth-child(7) .el-aside {
+      line-height: 320px;
+    }
+  }
+
+  .elContainerExample {
+    .el-header {
+      background-color: #B3C0D1;
+      color: #333;
+      line-height: 60px;
+    }
+    
+    .el-aside {
+      color: #333;
+    }
+  }
 
   .el-link {
     margin-right: 15px;
